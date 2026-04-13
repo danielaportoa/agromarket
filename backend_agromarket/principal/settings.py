56ac61059d21c7ecf0847e517ae62a6197e9a624
danielaportoa <danielaportoaguilera@gmail.com>
2026-04-13 14:23:05 -0400

    v1

--- backend_agromarket/principal/settings.py
+++ backend_agromarket/principal/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
