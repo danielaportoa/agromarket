07ed0a60dced704d01d57c61523d990ffe677e74
danielaportoa <danielaportoaguilera@gmail.com>
2026-04-14 17:24:46 -0400

    serializars

--- productos/serializers.py
+++ productos/serializers.py
@@ -12,17 +12,16 @@ class CertificacionSerializer(serializers.ModelSerializer):
         fields = ['id', 'nombre', 'descripcion', 'icono']
 
 class ProductoSerializer(serializers.ModelSerializer):
-    # Traemos los detalles de la categoría y certificaciones en lugar de solo el ID
     categoria = CategoriaSerializer(read_only=True)
     certificaciones = CertificacionSerializer(many=True, read_only=True)
     
-    # Exponemos la propiedad calculada de la huella de carbono
     huella_carbono_estimada = serializers.ReadOnlyField()
 
     class Meta:
         model = Producto
         fields = [
             'id', 'nombre', 'descripcion', 'precio', 'stock', 'imagen',
+            'imagen_url', 
             'categoria', 'certificaciones', 'es_certificado_organico',
             'kilometro_cero', 'distancia_km', 'huella_carbono_estimada',
             'fecha_creacion'
